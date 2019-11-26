6ddaf185d2918d8986d3c3b943ecf3d0200d3c20
joshua9889 <joshuatheinrich@gmail.com>
2019-11-26 11:48:07 -0500

    11/25 Late - 11/26 Early
    - Create OpenCV Pipeline to find the Skystone when it initial position.
    - Create Test Opmode to test pipeline
    - Uncomment out Cruise Code

--- TeamCode/src/main/java/com/team9889/lib/cruiseCode/CruiseCode.java
+++ TeamCode/src/main/java/com/team9889/lib/cruiseCode/CruiseCode.java
@@ -1,111 +1,112 @@
-//package com.team9889.lib.cruiseCode;
-//
-//import com.qualcomm.robotcore.util.RobotLog;
-////import com.team9889.ftc2019.auto.actions.Action;
-//import com.team9889.lib.android.FileReader;
-//import com.team9889.lib.android.FileWriter;
-//
-//import java.text.SimpleDateFormat;
-//import java.util.ArrayList;
-//import java.util.Date;
-//
-///**
-// * Created by joshua9889 on 8/28/2018.
-// */
-//
-//public class CruiseCode {
-//    private FileReader fileReader;
-//    private FileWriter fileWriter;
-//
-////    private ArrayList<Action> classes; // List of classes
-//
-//    public static void main(String... args){
-////        ArrayList<Action> actions = new ArrayList<>();
-//
-//        CruiseCode code = new CruiseCode("auto/test", actions);
-//        code.run();
-//    }
-//
-//    public CruiseCode(String fileName, ArrayList<Action> actions){
-//        fileReader = new FileReader(fileName + ".auto");
-//        fileWriter = new FileWriter(fileName+".log");
-//        classes = actions;
-//    }
-//
-//    private void run(){ // TODO: Make this thread safe
-//        String[] commandsFromFile = fileReader.lines();
-//
-//        // Iterate over all lines
-//        for (int i = 0; i < commandsFromFile.length; i++) {
-//            // Remove unneeded characters
-//            commandsFromFile[i] = commandsFromFile[i].replace(" ", ""); // Remove spaces
-//            commandsFromFile[i] = commandsFromFile[i].replace("\t", ""); // Remove tabs
-//            commandsFromFile[i] = commandsFromFile[i].replace("\n", ""); // Remove newlines
-//
-//            // Don't look for a valid command if the line is a comment
-//            if (!commandsFromFile[i].startsWith("#")) {
-//                // If Drive(10,0) is the command, splitting it will give us [Drive, 10,0)]
-//                String[] commands = commandsFromFile[i].split("\\(");
-//
-//                // Command Name from filename.auto
-//                commands[0] = commands[0].toLowerCase();
-//
-//                if(commands.length>=2){
-//                    // Check command list
-//                    for (int j = 0; j < classes.size(); j++) {
-//
-//                        // Current Action we are checking
-//                        Action currentClass = classes.get(j); // Current class to check
-//
-//                        // if commands[1] = 10,0), ["10", "0)"]
-//                        String[] parameters = commands[1].split(",");
-//
-//                        // remove ")" from last parameter
-//                        parameters[parameters.length-1] = parameters[parameters.length-1].replace(")", "");
-//
-//                        // Class name lower class
-//                        String actionNameLowerCase = currentClass.getClass().getSimpleName().toLowerCase();
-//
-//                        if (commands[0].equals(actionNameLowerCase)) {
-//                            String argsToAction = parameters[0];
-//                            for (int k = 1; k < parameters.length; k++) {
-//                                argsToAction += "," + parameters[k];
-//                            }
-//
-//                            String output = "Starting to Run " + currentClass.getClass().getSimpleName()
-//                                    + " at " + new SimpleDateFormat("hh:mm:ss:S MM.dd.yyyy").format(new Date());
-//
-//                            fileWriter.write(output);
-//                            try{RobotLog.d(output);}catch (Exception e){}
-//
-//                            // Setup
-//                            currentClass.setup(argsToAction);
-//
-//                            // Run
-//                            runAction(currentClass);
-//
-//                            output = "Finished Running " + currentClass.getClass().getSimpleName()
-//                                    + " at " + new SimpleDateFormat("hh:mm:ss:S MM.dd.yyyy").format(new Date());
-//
-//                            fileWriter.write(output);
-//                            try{RobotLog.d(output);}catch (Exception e){}
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        fileWriter.close();
-//        fileReader.close();
-//
-//    }
-//
-//    private void runAction(Action action){
-//        action.start();
-//
-//        while (!action.isFinished())
-//            action.update();
-//
-//        action.done();
-//    }
-//}
+package com.team9889.lib.cruiseCode;
+
+import com.qualcomm.robotcore.util.RobotLog;
+//import com.team9889.ftc2019.auto.actions.Action;
+import com.team9889.ftc2019.auto.actions.Action;
+import com.team9889.lib.android.FileReader;
+import com.team9889.lib.android.FileWriter;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by joshua9889 on 8/28/2018.
+ */
+
+public class CruiseCode {
+    private FileReader fileReader;
+    private FileWriter fileWriter;
+
+    private ArrayList<Action> classes; // List of classes
+
+    public static void main(String... args){
+        ArrayList<Action> actions = new ArrayList<>();
+
+        CruiseCode code = new CruiseCode("auto/test", actions);
+        code.run();
+    }
+
+    public CruiseCode(String fileName, ArrayList<Action> actions){
+        fileReader = new FileReader(fileName + ".auto");
+        fileWriter = new FileWriter(fileName+".log");
+        classes = actions;
+    }
+
+    private void run(){ // TODO: Make this thread safe
+        String[] commandsFromFile = fileReader.lines();
+
+        // Iterate over all lines
+        for (int i = 0; i < commandsFromFile.length; i++) {
+            // Remove unneeded characters
+            commandsFromFile[i] = commandsFromFile[i].replace(" ", ""); // Remove spaces
+            commandsFromFile[i] = commandsFromFile[i].replace("\t", ""); // Remove tabs
+            commandsFromFile[i] = commandsFromFile[i].replace("\n", ""); // Remove newlines
+
+            // Don't look for a valid command if the line is a comment
+            if (!commandsFromFile[i].startsWith("#")) {
+                // If Drive(10,0) is the command, splitting it will give us [Drive, 10,0)]
+                String[] commands = commandsFromFile[i].split("\\(");
+
+                // Command Name from filename.auto
+                commands[0] = commands[0].toLowerCase();
+
+                if(commands.length>=2){
+                    // Check command list
+                    for (int j = 0; j < classes.size(); j++) {
+
+                        // Current Action we are checking
+                        Action currentClass = classes.get(j); // Current class to check
+
+                        // if commands[1] = 10,0), ["10", "0)"]
+                        String[] parameters = commands[1].split(",");
+
+                        // remove ")" from last parameter
+                        parameters[parameters.length-1] = parameters[parameters.length-1].replace(")", "");
+
+                        // Class name lower class
+                        String actionNameLowerCase = currentClass.getClass().getSimpleName().toLowerCase();
+
+                        if (commands[0].equals(actionNameLowerCase)) {
+                            String argsToAction = parameters[0];
+                            for (int k = 1; k < parameters.length; k++) {
+                                argsToAction += "," + parameters[k];
+                            }
+
+                            String output = "Starting to Run " + currentClass.getClass().getSimpleName()
+                                    + " at " + new SimpleDateFormat("hh:mm:ss:S MM.dd.yyyy").format(new Date());
+
+                            fileWriter.write(output);
+                            try{RobotLog.d(output);}catch (Exception e){}
+
+                            // Setup
+                            currentClass.setup(argsToAction);
+
+                            // Run
+                            runAction(currentClass);
+
+                            output = "Finished Running " + currentClass.getClass().getSimpleName()
+                                    + " at " + new SimpleDateFormat("hh:mm:ss:S MM.dd.yyyy").format(new Date());
+
+                            fileWriter.write(output);
+                            try{RobotLog.d(output);}catch (Exception e){}
+                        }
+                    }
+                }
+            }
+        }
+
+        fileWriter.close();
+        fileReader.close();
+
+    }
+
+    private void runAction(Action action){
+        action.start();
+
+        while (!action.isFinished())
+            action.update();
+
+        action.done();
+    }
+}
---
+++ TeamCode/src/main/java/com/team9889/lib/detectors/ScanForSkyStonesPipeline.java
@@ -0,0 +1,160 @@
+package com.team9889.lib.detectors;
+
+import android.util.Log;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+/**
+ * Created by joshua9889 on 11/25/2019.
+ */
+public class ScanForSkyStonesPipeline extends OpenCvPipeline {
+
+    int positionOfSkyStone = 0;
+    boolean debug = false;
+    double upperPercentLimit = 0.55, lowerPercentLimit = 0.64;
+    int threshold = 60;
+
+
+    /**
+     * Default Config
+     */
+    public ScanForSkyStonesPipeline(){
+        this.debug = false;
+        this.upperPercentLimit = 0.55;
+        this.lowerPercentLimit = 0.64;
+        this.threshold = 60;
+    }
+
+    /**
+     * Custom Config
+     *
+     * @param debug
+     * @param upperPercentLimit
+     * @param lowerPercentLimit
+     * @param threshold
+     */
+    public ScanForSkyStonesPipeline(boolean debug, double upperPercentLimit, double lowerPercentLimit, int threshold) {
+        this.debug = false;
+        this.upperPercentLimit = upperPercentLimit;
+        this.lowerPercentLimit = lowerPercentLimit;
+        this.threshold = threshold;
+    }
+
+    /**
+     * @return Position of center of skystone
+     */
+    public double getPositionOfSkyStone() {
+        return positionOfSkyStone;
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        boolean rotate = true;
+        boolean scale = false;
+        boolean useGrayScale = false;
+        boolean drawRectangles = false;
+
+        Mat rotatedMat = new Mat();
+        if(rotate) {
+            Core.rotate(input, rotatedMat, Core.ROTATE_180); //ROTATE_180 or ROTATE_90_COUNTERCLOCKWISE
+        }
+
+        Mat scaledMat = new Mat();
+        if (scale) {
+            double scaleDownFactor = 4;
+
+            int height = input.height();
+            int width = input.width();
+
+            int scaledHeight = (int)(height / scaleDownFactor);
+            int scaledWidth = (int)(width / scaleDownFactor);
+
+            Size scaledSize = new Size(scaledWidth, scaledHeight);
+            Imgproc.resize(rotate ? rotatedMat : input, scaledMat, scaledSize, 0, 0, Imgproc.INTER_AREA);
+        } else {
+            (rotate ? rotatedMat : input).copyTo(scaledMat);
+        }
+
+        Mat grayScaleMat = new Mat();
+        if(useGrayScale) {
+            Imgproc.cvtColor(scaledMat, grayScaleMat, Imgproc.COLOR_BGR2GRAY);
+            Imgproc.cvtColor(grayScaleMat, grayScaleMat, Imgproc.COLOR_GRAY2RGB);
+        } else {
+            Imgproc.cvtColor(scaledMat, grayScaleMat, Imgproc.COLOR_BGR2RGB);
+        }
+
+        if (drawRectangles) {
+            Imgproc.rectangle(grayScaleMat, new Point(0, 0),
+                    new Point(grayScaleMat.width(), (int) (grayScaleMat.height() * upperPercentLimit)),
+                    new Scalar(0, 0, 0), 1);
+
+            Imgproc.rectangle(grayScaleMat, new Point(0, (int) (grayScaleMat.height() * lowerPercentLimit)),
+                    new Point(grayScaleMat.width(), grayScaleMat.height()),
+                    new Scalar(0, 0, 0), 1);
+        }
+
+        int columnCount = 0;
+        int columnsFound = 0;
+        for(int x=0; x<=grayScaleMat.width(); x++) {
+
+            double columnScore = 0;
+            for (int y = (int) (grayScaleMat.height() * upperPercentLimit); y <= (int) (grayScaleMat.height() * lowerPercentLimit); y++) {
+                try {
+                    double color = 0;
+                    for (double pixelScale : grayScaleMat.get(y, x)) {
+                        color += pixelScale/3.0;
+                    }
+
+                    if(color < threshold) {
+                        if(debug) grayScaleMat.put(y,x, 0, 255, 0);
+                        columnScore++;
+                    } else if(debug) {
+                        grayScaleMat.put(y, x, color, color, color);
+                    }
+
+                } catch (NullPointerException e) {
+                    Log.e("Camera: ", e.toString());
+                }
+            }
+
+            int heightOfScanningArea = (int)((grayScaleMat.height() * lowerPercentLimit)
+                    - (grayScaleMat.height() * upperPercentLimit));
+
+            int columnScoreForBlock = (int)(0.5 * heightOfScanningArea);
+            if (columnScore > columnScoreForBlock) {
+                if(debug) Imgproc.line(grayScaleMat,
+                        new Point(x, (int) (grayScaleMat.height() * upperPercentLimit)),
+                        new Point(x, (int) (grayScaleMat.height() * lowerPercentLimit)),
+                        new Scalar(0, 255, 0), 1);
+
+                columnCount++;
+            } else columnCount = 0;
+
+            if (columnCount > 5) {
+                if(debug) Imgproc.circle(grayScaleMat, new Point(x, (int)(heightOfScanningArea/2 + grayScaleMat.height() * upperPercentLimit)), 4, new Scalar(255, 0, 0), -1);
+                columnsFound++;
+                columnCount = 0;
+            }
+
+            if(columnsFound == 3) {
+                positionOfSkyStone = x;
+                Imgproc.circle(grayScaleMat, new Point(positionOfSkyStone,
+                                (int)(heightOfScanningArea/2 + grayScaleMat.height() * upperPercentLimit)),
+                        4, new Scalar(255, 0, 0), -1);
+            }
+        }
+
+        Imgproc.putText(grayScaleMat, "Pos: " + positionOfSkyStone, new Point(0,40),
+                1, 1, new Scalar(255, 0,0));
+        Imgproc.putText(grayScaleMat, "Col: " + columnsFound, new Point(0,50),
+                1, 1, new Scalar(255, 0,0));
+
+        return grayScaleMat;
+    }
+}
